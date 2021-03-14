e0d03e0c5a47f55be5f8052bc5025022d490c761
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-03-14 14:43:39 +0300

    class

---
+++ 05-class.ts
@@ -0,0 +1,25 @@
+interface Point{
+    x:number;
+    y:number;
+}
+
+interface Vehicle{
+    currentLocation: Point;
+    travelTo(point: Point): void;
+}
+
+class Taxi implements Vehicle{
+    //..
+    currentLocation: Point;
+    travelTo(point: Point): void{
+        console.log(`taksi x: ${point.x} konumundan y: ${point.y} konumuna gidiyor.`);
+    }
+}
+
+class Bus implements Vehicle{
+    //..
+    currentLocation: Point;
+    travelTo(point: Point): void{
+        console.log(`otobüs x: ${point.x} konumundan y: ${point.y} konumuna gidiyor.`);
+    }
+}
