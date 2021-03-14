8ec152f780ba7063d6d1b52e83c952a29d07724c
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-03-14 15:37:31 +0300

    properties

---
+++ 09-properties.ts
@@ -0,0 +1,46 @@
+interface Point{
+    x:number;
+    y:number;
+}
+
+interface Vehicle{
+    travelTo(point: Point): void;
+}
+
+class Taxi implements Vehicle{
+    
+    //private color: string;
+    //private currentLocation: Point;
+
+    /*
+    constructor(location?: Point,color?:string){
+        this.currentLocation= location;
+        this.color = color;
+    }
+    */
+
+    constructor(private _location?: Point,private color?:string){
+    }
+
+    travelTo(point: Point): void{
+        console.log(`taksi x: ${this._location.x} y: ${this._location.y} dan x: ${point.x} y: ${point.y} konumuna gidiyor.`);
+    }
+
+    get location(){
+        return this._location;
+    }
+
+    set location(value: Point){
+        if(value.x < 0 || value.y < 0){
+            throw new Error('Koordinat bilgileri negatif olamaz!');
+        }
+        this._location = value;
+    }
+}
+
+let taxi_1 : Taxi = new Taxi({x:2,y:5});
+//console.log(taxi_1.currentLocation.x) //ulaşılamaz;  
+//taxi_1.color = 'red'; //ulaşılamaz
+taxi_1.travelTo({x:10,y:20});
+let currentLocation = taxi_1.location;
+taxi_1.location = ({x:2,y:5});
